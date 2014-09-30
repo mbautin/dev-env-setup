d1cb61716945850195d88405861dd204166395f1
Mikhail Bautin <mbautin@osiris.clearstorydatainc.com>
2014-09-30 13:36:00 -0700

    Use git prompt

--- run_from_bashrc.sh
+++ run_from_bashrc.sh
@@ -2,4 +2,19 @@
 
 # This should be invoked from ~/.bashrc.
 
+# -------------------------------------------------------------------------------- 
+# gitprompt configuration
+# -------------------------------------------------------------------------------- 
+
+# Set config variables first
+GIT_PROMPT_ONLY_IN_REPO=1
+
+# GIT_PROMPT_FETCH_REMOTE_STATUS=0   # uncomment to avoid fetching remote status
+
+# GIT_PROMPT_START=...    # uncomment for custom prompt start sequence
+# GIT_PROMPT_END=...      # uncomment for custom prompt end sequence
+
+# as last entry source the gitprompt script
+source ~/.bash-git-prompt/gitprompt.sh
+
 
