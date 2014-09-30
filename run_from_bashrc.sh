bd6efe1e574e5304fbe97fb96778029d4d412ea7
Mikhail Bautin <mbautin@osiris.clearstorydatainc.com>
2014-09-30 14:06:49 -0700

    Tweak prompt

--- run_from_bashrc.sh
+++ run_from_bashrc.sh
@@ -11,7 +11,8 @@ GIT_PROMPT_ONLY_IN_REPO=0
 
 # GIT_PROMPT_FETCH_REMOTE_STATUS=0   # uncomment to avoid fetching remote status
 
-# GIT_PROMPT_START=...    # uncomment for custom prompt start sequence
+GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${White}${HOSTNAME%%.*}:${Yellow}${PathShort}${ResetColor}"
+
 # GIT_PROMPT_END=...      # uncomment for custom prompt end sequence
 
 # as last entry source the gitprompt script
