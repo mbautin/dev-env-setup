6176fecea0224ac4e41808208986b0e7f9b424aa
Mikhail Bautin <mbautin@osiris.clearstorydatainc.com>
2014-09-30 13:38:39 -0700

    Configure bash-git-prompt

--- run_from_bashrc.sh
+++ run_from_bashrc.sh
@@ -7,7 +7,7 @@
 # -------------------------------------------------------------------------------- 
 
 # Set config variables first
-GIT_PROMPT_ONLY_IN_REPO=1
+GIT_PROMPT_ONLY_IN_REPO=0
 
 # GIT_PROMPT_FETCH_REMOTE_STATUS=0   # uncomment to avoid fetching remote status
 
@@ -15,6 +15,7 @@ GIT_PROMPT_ONLY_IN_REPO=1
 # GIT_PROMPT_END=...      # uncomment for custom prompt end sequence
 
 # as last entry source the gitprompt script
-source ~/.bash-git-prompt/gitprompt.sh
+export _DEV_ENV_SETUP_DIR=$( dirname "${BASH_SOURCE}" )
+source $_DEV_ENV_SETUP_DIR/bash-git-prompt/gitprompt.sh
 
 
