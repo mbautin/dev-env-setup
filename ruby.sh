bf0bdc3024687cac8a558955e333e0bc284b7570
Mikhail Bautin <mbautin@osiris.clearstorydatainc.com>
2014-09-30 13:25:21 -0700

    Update Ruby version

--- ruby.sh
+++ ruby.sh
@@ -3,7 +3,7 @@
 set -e -u -o pipefail -x
 
 curl -sSL https://get.rvm.io | bash -s stable
-ruby_version=1.9.3-p484
+ruby_version=2.1.2
 
 set +u +x
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
